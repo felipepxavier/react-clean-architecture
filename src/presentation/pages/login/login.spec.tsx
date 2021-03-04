025401640195ebdcbc9cd25e0a7c8d06e9b5d5ec
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2021-03-03 22:42:15 -0300

    test: ensure Login present valid state if password is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -41,10 +41,8 @@ describe('Login component', () => {
 
   test('Should show email error if Validation fails', () => {
     const { sut, validationStub } = makeSut()
-
     const emailInput = sut.getByTestId('email')
     fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
-
     const emailStatus = sut.getByTestId('email-status')
     expect(emailStatus.title).toBe(validationStub.errorMessage)
     expect(emailStatus.textContent).toBe('🔴')
@@ -52,12 +50,20 @@ describe('Login component', () => {
 
   test('Should show password error if Validation fails', () => {
     const { sut, validationStub } = makeSut()
-
     const passwordInput = sut.getByTestId('password')
     fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
-
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe(validationStub.errorMessage)
     expect(passwordStatus.textContent).toBe('🔴')
+  })
+
+  test('Should show valid password state if Validation succeeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const passwordStatus = sut.getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Tudo certo!')
+    expect(passwordStatus.textContent).toBe('🟢')
   })
 })
