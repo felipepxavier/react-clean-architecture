2cd2db81dd92e354625fd61e180a1a22a6911098
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2021-07-18 13:19:53 -0300

    refactor: move validation test folder to correct place

--- src/validation/validators/validation-composite/validation-composite.ts
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Validation } from '@/presentation/protocols/validation'
 import { FieldValidation } from '@/validation/protocols/field-validation'
 
